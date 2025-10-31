232bbcd36fbadde47691b018e8b8dce65cffcde8
sharansudheer <saran_sudheer@hotmail.com>
2025-10-31 11:56:14 +0530

    push:logging
    logging trails

--- app/DHS/DinahMadani.py
+++ app/DHS/DinahMadani.py
@@ -1,2 +1,22 @@
 import datetime
 import logging
+import sys
+
+def setup_custom_logger(name):
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.FileHandler('log.txt', mode='w')
+    handler.setFormatter(formatter)
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    logger.addHandler(screen_handler)
+    return logger
+
+>>> logger = setup_custom_logger('myapp')
+>>> logger.info('This is a message!')
+2015-02-04 15:07:12 INFO     This is a message!
+>>> logger.error('Here is another')
+2015-02-04 15:07:30 ERROR    Here is another
